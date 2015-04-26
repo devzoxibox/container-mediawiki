6495ce00279a5d7a20bb8cb185d24bf554fdb022
zoxibox <djzoxi@gmail.com>
2015-04-26 21:25:22 +0200

    Update start-mysqld.sh

--- scripts/start-mysqld.sh
+++ scripts/start-mysqld.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Copie des DB dans le volumes /database
 if [ ! -f "/database/ibdata1" ]; then
 
 	# copy over customised config
@@ -8,5 +9,5 @@ if [ ! -f "/database/ibdata1" ]; then
 fi
 
 
-
+# Démarrage de mysql
 exec mysqld_safe --datadir=/database
