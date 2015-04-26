52c4e57cb2254d1c988a2aacd75a639e5f18fa66
zoxibox <djzoxi@gmail.com>
2015-04-26 20:34:39 +0200

    Update start-mysqld.sh

--- scripts/start-mysqld.sh
+++ scripts/start-mysqld.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-cp -R /var/lib/mysql/* /database
+if [ ! -f "/database/ibdata1" ]; then
+
+	# copy over customised config
+	cp -R /var/lib/mysql/* /database
+				
+fi
+
+
 
 exec mysqld_safe --datadir=/database
