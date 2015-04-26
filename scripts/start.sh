a22e470da7478b7980b46c776f11d1c07ffa3730
zoxibox <djzoxi@gmail.com>
2015-04-26 20:38:21 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-if [ ! -f "/config/index.php" ]; then
-
-	# copy over customised config
-	cp -R /usr/share/mediawiki/* /config
-				
-fi
-
-
 # Démarrage
 exec supervisord -n
-#exec mysqld_safe
-#exec /usr/sbin/apache2 -D FOREGROUND
+
 
