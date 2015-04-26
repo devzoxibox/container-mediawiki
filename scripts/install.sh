87fa0a6b7e53b0ca7c8f06250365f752deb53907
zoxibox <djzoxi@gmail.com>
2015-04-26 21:39:29 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Ajout du mdp mysql
-echo 'mysql-server mysql-server/root_password password $MYSQL_PASS' | debconf-set-selections
-echo 'mysql-server mysql-server/root_password_again password $MYSQL_PASS' | debconf-set-selections
+echo 'mysql-server mysql-server/root_password password password' | debconf-set-selections
+echo 'mysql-server mysql-server/root_password_again password password' | debconf-set-selections
 
 # Ajout du mdp et config phpmyadmin
 echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
-echo 'phpmyadmin phpmyadmin/app-password-confirm password $MYSQL_PASS' | debconf-set-selections
-echo 'phpmyadmin phpmyadmin/mysql/admin-pass password $MYSQL_PASS' | debconf-set-selections
-echo 'phpmyadmin phpmyadmin/mysql/app-pass password $MYSQL_PASS' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/app-password-confirm password password' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/admin-pass password password' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/app-pass password password' | debconf-set-selections
 echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections
 
 # Installation de supervisor mediawiki
