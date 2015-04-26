f159555d830e2b7095ed974e5a4d4465a16de936
zoxibox <djzoxi@gmail.com>
2015-04-26 21:24:02 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,10 +11,10 @@ echo 'phpmyadmin phpmyadmin/mysql/admin-pass password $MYSQL_PASS' | debconf-set
 echo 'phpmyadmin phpmyadmin/mysql/app-pass password $MYSQL_PASS' | debconf-set-selections
 echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections
 
-# Installation de phpmyadmin mediawiki
+# Installation de supervisor mediawiki
 apt-get update && apt-get -q -y install supervisor mediawiki php-apc 
 
-
+# Démarrage mysql pour installation auto phpmyadmin
 service mysql start && apt-get -q -y install phpmyadmin
 
 
@@ -24,6 +24,6 @@ rm /etc/apache2/conf.d/mediawiki.conf
 rm -R /etc/apache2/sites-enabled/*
 rm -R /etc/apache2/sites-available/*
 
-
+# Droits pour apache2 et mysql
 chown -R nobody:users /var/log/apache2/
 chown -R nobody:users /var/lib/mysql
