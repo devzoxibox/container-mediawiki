9e56dd422b7de38235685bfee125ef81c7c64932
zoxibox <djzoxi@gmail.com>
2015-04-26 20:43:11 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# déplacement config mediawiki
+if [ ! -f "/config/index.php" ]; then
+
+	# copy over customised config
+	cp -R /usr/share/mediawiki/* /config
+				
+fi
+
 # Démarrage
 exec supervisord -n
-
-
